a0650c273d403e6db23ef2b5c77e3946d4b46318
kareenap55 <kareenap1412@gmail.com>
2025-04-05 01:57:00 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+var currentUrl = window.location.href; // Get the current URL
+var urlLength = currentUrl.length;     // Get the length of the URL
+alert("The length of the URL is: " + urlLength); // Show the result
